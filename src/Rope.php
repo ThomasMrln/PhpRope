e270e1221195191f37dff98777075ab2f470e7e8
ThomasMrln <thomas@merlen.fr>
2014-03-20 09:33:53 +0100

    add src folder

---
+++ src/Rope.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ThomasMrln\Rope;
+
+/**
+ *	@resume		Rope is a php class with basic function for date/url/string format we can use every time.
+**/
+
+class Rope {
+	
+	public static function current_url() {
+			$url	=	'http://'.preg_replace('/\?.{1,}/', '', $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+			if ($url[ strlen($url)-1 ] != '/')		$url 	.=	'/';
+			
+			return $url;
+	}
+	
+}
